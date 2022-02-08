0820a78f4d18756a1c7a074726b001bb1c1e42b9
Alexander Krylov <71841167+confusedqqw@users.noreply.github.com>
2022-02-09 00:26:58 +0300

    Update task6.java

--- laba1/src/task6.java
+++ laba1/src/task6.java
@@ -6,21 +6,9 @@ public class task6 {
         int b = InputScanner.enterIntFromConsole();
         System.out.println("Enter c: ");
         int c = InputScanner.enterIntFromConsole();
-        int min=0;
-        int max;
-        if(a > b) {
-            max = a;
-            min = b;
-        }else {
-            max = b;
-            min = a;
-        }
-        if(c > max) {
-            max = c;
-        }
-        if(c<a || c<b)
-            min = c;
-        System.out.println("max = " + max);
-        System.out.println("min = " + min);
+
+        int result = Math.max(a, Math.max(b, c)) + Math.min(a, Math.min(b, c));
+        System.out.println(result);
     }
 }
+
